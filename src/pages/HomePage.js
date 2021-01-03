00ce09b6fca342923d256a23f878d401b32713e7
alfredzhuang <alfredzhuang123@gmail.com>
2021-01-03 00:16:29 -0800

    added padding to home page and changed button font/text

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,7 @@ function HomePage(props) {
             <motion.div className="headline-container" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
                 <h1 className ="heading">{props.title}</h1>
                 <h2>{props.subTitle}</h2>
-                <Button size="lg" variant="outline-info" target="_blank" href="/resume.pdf" rel="noreferrer">Resume</Button>
+                <Button class="btn" size="lg" variant="outline-info" target="_blank" href="/resume.pdf" rel="noreferrer">My Resume</Button>
             </motion.div>
             <motion.div className="about-container" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
                 <div className = "headline-img">
