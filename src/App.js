84b941cd8fdd87656146e6b994e0c2ecbf974712
alfredzhuang <alfredzhuang123@gmail.com>
2021-01-07 16:48:09 -0800

    separated about me section from home into its own page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import BarLoader from "react-spinners/BarLoader";
 
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import ProjectsPage from './pages/ProjectsPage';
 import ContactPage from './pages/ContactPage';
 
@@ -25,11 +26,13 @@ class App extends React.Component {
       ],
       home: {
         title: 'Alfred Zhuang',
-        subTitle: 'An Aspiring Software Engineer',
+        subTitle: 'Computer Science Major',
+      },
+      about: {
         about: {
           subTitle1: 'About me',
-          subText1: 'I\'m a 19 year old Computer Science Major at Cal Poly Pomona. In 2018, I started out coding by learning Java. Since then, I\'ve expanded my pool of coding languages with Javascript and Flutter. Currently, I am learning full stack web development and hope to become a SWE to put my skills to the test.',
-          subText2: 'I enjoy designing and creating projects, with the goal of bringing joy and convenience for others with every application I create.',
+          subText1: 'I\'m a 19 year old Computer Science Major at Cal Poly Pomona. In 2018, I started out coding by learning Java. Since then, I\'ve expanded my pool of coding languages with Javascript and Flutter. Currently, I am learning full stack web development and hope to one day become a SWE to put my skills to the test.',
+          subText2: 'I enjoy designing and creating projects, with the goal of bringing joy and convenience for others in every application that I create.',
           subText3: 'In my free time, in addition to coding, I enjoy keeping busy by spending time on my hobbies such as: working out, playing videogames, cutting hair, learning mandarin and cooking.',
         },
         skills: {
@@ -110,14 +113,16 @@ class App extends React.Component {
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
                 <Link className="nav-link text" to="/">Home</Link>
+                <Link className="nav-link text" to="/about">About</Link>
                 <Link className="nav-link text" to="/projects">Projects</Link>
                 <Link className="nav-link text" to="/contact">Contact</Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
-          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} about={this.state.home.about} skills={this.state.home.skills}/>} />
-          <Route path="/projects" exact render={() => <ProjectsPage title={this.state.projects.title} zoomin={this.state.projects.zoomin} lookingForGroup={this.state.projects.lookingForGroup} twitterTracker={this.state.projects.twitterTracker} dragonfistFitness={this.state.projects.dragonfistFitness} valuableVocabulary={this.state.projects.valuableVocabulary}/>} />
-          <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title}/>} />
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} />} />
+          <Route path="/about" exact render={() => <AboutPage about={this.state.about.about} skills={this.state.about.skills} />} />
+          <Route path="/projects" exact render={() => <ProjectsPage title={this.state.projects.title} zoomin={this.state.projects.zoomin} lookingForGroup={this.state.projects.lookingForGroup} twitterTracker={this.state.projects.twitterTracker} dragonfistFitness={this.state.projects.dragonfistFitness} valuableVocabulary={this.state.projects.valuableVocabulary} />} />
+          <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} />} />
 
           <Footer/>
 
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './AboutPage.css';
+import { FaReact, FaNodeJs, FaJava } from "react-icons/fa";
+import { SiJavascript, SiFlutter, SiHtml5, SiCss3 } from "react-icons/si";
+import { motion } from "framer-motion";
+
+function AboutPage(props) {
+    return (
+        <div className="about">
+            <motion.div className="about-container" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+                <div className = "headline-img">
+                    <img className = "img" src="/images/Alfred.jpg" alt = "Alfred image"/>
+                </div>
+                <div className="profile">
+                    <h2>{props.about.subTitle1}</h2>
+                    <p>{props.about.subText1}</p>
+                    <p>{props.about.subText2}</p>
+                    <p>{props.about.subText3}</p>
+                </div>
+             </motion.div>
+             <motion.div className="skills-container" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+                <h3>{props.skills.subTitle1}</h3>
+                <p>{props.skills.subText1}</p>
+                <ul class="skills">
+                    <li><FaJava className="skills-icon" /> {props.skills.java}</li>
+                    <li><SiHtml5 className="skills-icon" /> {props.skills.html}</li>
+                    <li><SiCss3 className="skills-icon" /> {props.skills.css}</li>
+                    <li><SiJavascript className="skills-icon" /> {props.skills.javascript}</li>
+                    <li><FaReact className="skills-icon" /> {props.skills.react}</li>
+                    <li><FaNodeJs className="skills-icon" /> {props.skills.node}</li>
+                    <li><SiFlutter className="skills-icon" /> {props.skills.flutter}</li>
+                </ul>
+             </motion.div>
+        </div>
+    );
+}
+
+export default AboutPage;
