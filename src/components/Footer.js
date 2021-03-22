bc2a204d3c9143f58a0c8d0c2e679d6c7390a9e6
alfredzhuang <alfredzhuang123@gmail.com>
2021-03-22 15:26:00 -0700

    move resume button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import './Footer.css'
-import { SiLinkedin, SiGithub } from 'react-icons/si';
-import { MdEmail } from 'react-icons/md';
+import React from "react";
+import "./Footer.css";
+import { SiLinkedin, SiGithub } from "react-icons/si";
+import { AiFillFileText } from "react-icons/ai";
 
 function Footer() {
-
-    return (
-        <footer className="footer fixed-bottom" bg="transparent">
-            <div className = "container">
-                <a class="icon" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/alfred-zhuang/">
-                    <SiLinkedin className="footer-icon1"/>
-                </a>
-                <a class="icon" rel="noreferrer" target="_blank" href="mailto:alfredzhuang1234@gmail.com">
-                    <MdEmail className="footer-icon2"/>
-                </a>
-                <a class="icon" rel="noreferrer" target="_blank" href="https://github.com/alfredzhuang">
-                    <SiGithub className="footer-icon3"/>
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer fixed-bottom" bg="transparent">
+      <div className="container">
+        <a
+          class="icon"
+          rel="noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/alfred-zhuang/"
+        >
+          <SiLinkedin className="footer-icon1" />
+        </a>
+        <a class="icon" rel="noreferrer" target="_blank" href="/resume.pdf">
+          <AiFillFileText className="footer-icon2" />
+        </a>
+        <a
+          class="icon"
+          rel="noreferrer"
+          target="_blank"
+          href="https://github.com/alfredzhuang"
+        >
+          <SiGithub className="footer-icon3" />
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./HomePage.css";
 import Button from "react-bootstrap/Button";
 import { motion } from "framer-motion";
-import { FaRegFileAlt } from "react-icons/fa";
 
 function HomePage(props) {
   return (
@@ -16,18 +15,6 @@ function HomePage(props) {
         <h3 class="top-subheading">{props.subTitle1}</h3>
         <h1 className="heading">{props.title}</h1>
         <h2 class="bottom-subheading">{props.subTitle2}</h2>
-        <Button
-          className="btn btn-info"
-          size="md"
-          target="_blank"
-          href="/resume.pdf"
-          rel="noreferrer"
-        >
-            <div className="button-icon">
-            <FaRegFileAlt /> 
-            </div>
-            <span className="button-content">My Resume</span>
-        </Button>
       </motion.div>
     </div>
   );
